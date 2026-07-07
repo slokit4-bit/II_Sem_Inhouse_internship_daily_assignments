0a6a81784817c943fd7dc52f4b7ef6c012f8fc60
slokit4-bit <slokit4@gmail.com>
2026-07-07 22:12:23 +0530

    Add files via upload

---
+++ day7/first.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Web Page</title>
+</head>
+<body>
+    <h1>Welcome to My Web Page</h1>
+<?php
+// echo "Hello World!";
+//phpinfo();
+
+//echo date("Y-m-d");
+//echo $_SERVER['HTTP_USER_AGENT'];
+$name = "John";
+echo $name;
+
+$student = ["John", "Doe", 25];
+echo $student[0];
+?> 
+</body>
+</html>
