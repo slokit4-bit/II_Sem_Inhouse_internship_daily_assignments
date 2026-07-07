4ac93bcf33dca26de6d390d96a33d49dc2da6b53
slokit4-bit <slokit4@gmail.com>
2026-07-07 22:14:11 +0530

    Add files via upload

---
+++ day8/third.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Priya Sharma";
+
+$college = "MIT Pune";
+$branch = "Computer Engineering";
+?>
+
+<h1>Hello, <?php echo $name; ?></h1>
+<p> <?php echo $college; ?> | <?php echo $branch; ?> </p>
